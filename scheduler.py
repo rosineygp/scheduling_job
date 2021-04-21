3dc8a3ea2f6bd87fbbfd5b6df148a3dba194e89a
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-04-21 07:48:27 -0300

    chore: just format code with default autopep8

--- scheduler.py
+++ scheduler.py
@@ -4,158 +4,160 @@ import re
 
 
 def str_to_time(strtime):
-  """
-  Convert str('%Y-%m-%d %H:%M:%S') to datetime.
+    """
+    Convert str('%Y-%m-%d %H:%M:%S') to datetime.
 
-  Parameters:
-  strtime (str): date and time
+    Parameters:
+    strtime (str): date and time
 
-  Returns:
-  datetime: Object datetime
-  """
-  return datetime.strptime(strtime, '%Y-%m-%d %H:%M:%S')
+    Returns:
+    datetime: Object datetime
+    """
+    return datetime.strptime(strtime, '%Y-%m-%d %H:%M:%S')
 
 
 def str_natural_to_time(str):
-  """
-  Convert natural brazilian time string to dict.
+    """
+    Convert natural brazilian time string to dict.
 
-  eg. (2 horas e 30 minutos, 1 hora, 8 horas,...)
+    eg. (2 horas e 30 minutos, 1 hora, 8 horas,...)
 
-  Parameters:
-  str (str): Brazilian natural hours
+    Parameters:
+    str (str): Brazilian natural hours
 
-  Returns:
-  dict:
-    int: hours
-    int: minutes
-    int: seconds
-    datetime: Object datetime
-  """
-  hours = 0
-  minutes = 0
-  seconds = 0
+    Returns:
+    dict:
+      int: hours
+      int: minutes
+      int: seconds
+      datetime: Object datetime
+    """
+    hours = 0
+    minutes = 0
+    seconds = 0
 
-  tokens = str.split()
+    tokens = str.split()
 
-  for i in range(len(tokens)):
-    try:
-      n = int(tokens[i])
+    for i in range(len(tokens)):
+        try:
+            n = int(tokens[i])
 
-      time_str = tokens[i + 1]
-      if re.search('^hora(s|)(,|)$', time_str):
-        hours = n
-      elif re.search('^minuto(s|)(,|)$', time_str):
-        minutes = n
-      elif re.search('^segundo(s|)(,|)$', time_str):
-        seconds = n
-    except:
-      pass
+            time_str = tokens[i + 1]
+            if re.search('^hora(s|)(,|)$', time_str):
+                hours = n
+            elif re.search('^minuto(s|)(,|)$', time_str):
+                minutes = n
+            elif re.search('^segundo(s|)(,|)$', time_str):
+                seconds = n
+        except:
+            pass
 
-  return {
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds,
-      'datetime': datetime.strptime(f"{hours}:{minutes}:{seconds}", "%H:%M:%S")
-  }
+    return {
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+        'datetime': datetime.strptime(f"{hours}:{minutes}:{seconds}", "%H:%M:%S")
+    }
 
 
 def make_window_filter(ini, end):
-  """
-  Set window parameters for window_filter function.
-
-  Parameters:
-  ini (datetime): the beginning of schedule window
-  end (datetime): the end of schedule window
-
-  Returns:
-  window_filter (lambda): Parametrized sort function
-  """
-
-  def window_filter(item):
     """
-    Filter jobs between ini and end execution window.
+    Set window parameters for window_filter function.
 
     Parameters:
-    item(dict): single job dict
+    ini (datetime): the beginning of schedule window
+    end (datetime): the end of schedule window
 
     Returns:
-    Boolean: True if the job is in window execution interval, otherwise false.
+    window_filter (lambda): Parametrized sort function
     """
-    if item['Data Máxima de conclusão'] >= ini and item['Data Máxima de conclusão'] <= end:
-      return True
 
-    return False
-  return window_filter
+    def window_filter(item):
+        """
+        Filter jobs between ini and end execution window.
+
+        Parameters:
+        item(dict): single job dict
+
+        Returns:
+        Boolean: True if the job is in window execution interval, otherwise false.
+        """
+        if item['Data Máxima de conclusão'] >= ini and item['Data Máxima de conclusão'] <= end:
+            return True
+
+        return False
+    return window_filter
 
 
 def sum_until_max(slice, max):
-  """
-  Sum slice of jobs and break if is lt max
+    """
+    Sum slice of jobs and break if is lt max
 
-  Parameters:
-  slice (array): List of jobs
-  max (dict): Max windows execution
+    Parameters:
+    slice (array): List of jobs
+    max (dict): Max windows execution
 
-  Returns:
-  array: Array with jobs ids (daily window)
-  """
-  hours = 0
-  minutes = 0
-  seconds = 0
+    Returns:
+    array: Array with jobs ids (daily window)
+    """
+    hours = 0
+    minutes = 0
+    seconds = 0
 
-  result = []
+    result = []
 
-  for i, item in enumerate(slice):
-    hours += item['Tempo estimado']['hours']
-    minutes += item['Tempo estimado']['minutes']
-    seconds += item['Tempo estimado']['seconds']
+    for i, item in enumerate(slice):
+        hours += item['Tempo estimado']['hours']
+        minutes += item['Tempo estimado']['minutes']
+        seconds += item['Tempo estimado']['seconds']
 
-    t = datetime.strptime(f"{hours}:{minutes}:{seconds}", "%H:%M:%S")
+        t = datetime.strptime(f"{hours}:{minutes}:{seconds}", "%H:%M:%S")
 
-    if t <= max:
-      result.append(item['ID'])
-    else:
-      break
+        if t <= max:
+            result.append(item['ID'])
+        else:
+            break
 
-  return result
+    return result
 
 
 if __name__ == "__main__":
-  with open('jobs.json') as json_file:
-    data = json.load(json_file)
+    with open('jobs.json') as json_file:
+        data = json.load(json_file)
 
-  # normalize data
-  for i in data:
-    i['Data Máxima de conclusão'] = str_to_time(i['Data Máxima de conclusão'])
-    i['Tempo estimado'] = str_natural_to_time(i['Tempo estimado'])
+    # normalize data
+    for i in data:
+        i['Data Máxima de conclusão'] = str_to_time(
+            i['Data Máxima de conclusão'])
+        i['Tempo estimado'] = str_natural_to_time(i['Tempo estimado'])
 
-  # sort data by 'Data Máxima de conclusão'
-  data = sorted(data, key=lambda k: k['Data Máxima de conclusão'])
+    # sort data by 'Data Máxima de conclusão'
+    data = sorted(data, key=lambda k: k['Data Máxima de conclusão'])
 
-  # generate initial max_time like docs
-  max_time = str_natural_to_time('8 horas')
+    # generate initial max_time like docs
+    max_time = str_natural_to_time('8 horas')
 
-  # remove jobs outside execution window
-  ini = str_to_time("2019-11-10 09:00:00")
-  end = str_to_time("2019-11-11 12:00:00")
+    # remove jobs outside execution window
+    ini = str_to_time("2019-11-10 09:00:00")
+    end = str_to_time("2019-11-11 12:00:00")
 
-  wf = make_window_filter(ini, end)
-  filtered = list(filter(wf, data))
+    wf = make_window_filter(ini, end)
+    filtered = list(filter(wf, data))
 
-  out = []
-  i = 0
-  while i < len(filtered):
-    item = filtered[i]
-    if item['Tempo estimado']['datetime'] > max_time['datetime']:
-      print(f"o job '{item['ID']}'  não pode ser processado > janela diaria!")
-      i += 1
-    elif item['Tempo estimado']['datetime'] == max_time['datetime']:
-      out.append([item['ID']])
-      i += 1
-    else:
-      items = sum_until_max(filtered[i:], max_time['datetime'])
-      out.append(items)
-      i += len(items)
+    out = []
+    i = 0
+    while i < len(filtered):
+        item = filtered[i]
+        if item['Tempo estimado']['datetime'] > max_time['datetime']:
+            print(
+                f"o job '{item['ID']}'  não pode ser processado > janela diaria!")
+            i += 1
+        elif item['Tempo estimado']['datetime'] == max_time['datetime']:
+            out.append([item['ID']])
+            i += 1
+        else:
+            items = sum_until_max(filtered[i:], max_time['datetime'])
+            out.append(items)
+            i += len(items)
 
-  print(out)
+    print(out)
--- test_scheduler.py
+++ test_scheduler.py
@@ -6,102 +6,102 @@ import scheduler
 
 
 class TestScheduler(unittest.TestCase):
-  def test_str_to_time(self):
-    d = scheduler.str_to_time('2019-11-10 12:00:00')
-    assert(d.year == 2019)
-    assert(d.month == 11)
-    assert(d.day == 10)
-    assert(d.hour == 12)
-    assert(d.minute == 0)
-    assert(d.second == 0)
+    def test_str_to_time(self):
+        d = scheduler.str_to_time('2019-11-10 12:00:00')
+        assert(d.year == 2019)
+        assert(d.month == 11)
+        assert(d.day == 10)
+        assert(d.hour == 12)
+        assert(d.minute == 0)
+        assert(d.second == 0)
 
-  def test_str_natural_to_time(self):
-    d = scheduler.str_natural_to_time('1 hora')
-    assert(d['hours'] == 1)
-    assert(d['minutes'] == 0)
-    assert(d['seconds'] == 0)
-    assert(d['datetime'].hour == 1)
-    assert(d['datetime'].minute == 0)
-    assert(d['datetime'].second == 0)
+    def test_str_natural_to_time(self):
+        d = scheduler.str_natural_to_time('1 hora')
+        assert(d['hours'] == 1)
+        assert(d['minutes'] == 0)
+        assert(d['seconds'] == 0)
+        assert(d['datetime'].hour == 1)
+        assert(d['datetime'].minute == 0)
+        assert(d['datetime'].second == 0)
 
-    d = scheduler.str_natural_to_time('2 horas')
-    assert(d['hours'] == 2)
-    assert(d['minutes'] == 0)
-    assert(d['seconds'] == 0)
-    assert(d['datetime'].hour == 2)
-    assert(d['datetime'].minute == 0)
-    assert(d['datetime'].second == 0)
+        d = scheduler.str_natural_to_time('2 horas')
+        assert(d['hours'] == 2)
+        assert(d['minutes'] == 0)
+        assert(d['seconds'] == 0)
+        assert(d['datetime'].hour == 2)
+        assert(d['datetime'].minute == 0)
+        assert(d['datetime'].second == 0)
 
-    d = scheduler.str_natural_to_time('4 horas e 10 minutos')
-    assert(d['hours'] == 4)
-    assert(d['minutes'] == 10)
-    assert(d['seconds'] == 0)
-    assert(d['datetime'].hour == 4)
-    assert(d['datetime'].minute == 10)
-    assert(d['datetime'].second == 0)
+        d = scheduler.str_natural_to_time('4 horas e 10 minutos')
+        assert(d['hours'] == 4)
+        assert(d['minutes'] == 10)
+        assert(d['seconds'] == 0)
+        assert(d['datetime'].hour == 4)
+        assert(d['datetime'].minute == 10)
+        assert(d['datetime'].second == 0)
 
-    d = scheduler.str_natural_to_time('6 horas, 20 minutos e 35 segundos')
-    assert(d['hours'] == 6)
-    assert(d['minutes'] == 20)
-    assert(d['seconds'] == 35)
-    assert(d['datetime'].hour == 6)
-    assert(d['datetime'].minute == 20)
-    assert(d['datetime'].second == 35)
+        d = scheduler.str_natural_to_time('6 horas, 20 minutos e 35 segundos')
+        assert(d['hours'] == 6)
+        assert(d['minutes'] == 20)
+        assert(d['seconds'] == 35)
+        assert(d['datetime'].hour == 6)
+        assert(d['datetime'].minute == 20)
+        assert(d['datetime'].second == 35)
 
-    d = scheduler.str_natural_to_time('1 hora, 20 minutos e 35 segundos')
-    assert(d['hours'] == 1)
-    assert(d['minutes'] == 20)
-    assert(d['seconds'] == 35)
-    assert(d['datetime'].hour == 1)
-    assert(d['datetime'].minute == 20)
-    assert(d['datetime'].second == 35)
+        d = scheduler.str_natural_to_time('1 hora, 20 minutos e 35 segundos')
+        assert(d['hours'] == 1)
+        assert(d['minutes'] == 20)
+        assert(d['seconds'] == 35)
+        assert(d['datetime'].hour == 1)
+        assert(d['datetime'].minute == 20)
+        assert(d['datetime'].second == 35)
 
-  def test_scheduler_windows_filter(self):
+    def test_scheduler_windows_filter(self):
 
-    with open('jobs.json') as json_file:
-      data = json.load(json_file)
+        with open('jobs.json') as json_file:
+            data = json.load(json_file)
 
-      for i in data:
-        i['Data Máxima de conclusão'] = scheduler.str_to_time(
-            i['Data Máxima de conclusão'])
+            for i in data:
+                i['Data Máxima de conclusão'] = scheduler.str_to_time(
+                    i['Data Máxima de conclusão'])
 
-      ini = scheduler.str_to_time("2019-11-10 09:00:00")
-      end = scheduler.str_to_time("2019-11-11 12:00:00")
-      wf = scheduler.make_window_filter(ini, end)
-      f = list(filter(wf, data))
-      assert(len(f) == 3)
+            ini = scheduler.str_to_time("2019-11-10 09:00:00")
+            end = scheduler.str_to_time("2019-11-11 12:00:00")
+            wf = scheduler.make_window_filter(ini, end)
+            f = list(filter(wf, data))
+            assert(len(f) == 3)
 
-      ini = scheduler.str_to_time("2019-11-10 09:00:00")
-      end = scheduler.str_to_time("2019-11-10 12:00:00")
-      wf = scheduler.make_window_filter(ini, end)
-      f = list(filter(wf, data))
-      assert(len(f) == 1)
+            ini = scheduler.str_to_time("2019-11-10 09:00:00")
+            end = scheduler.str_to_time("2019-11-10 12:00:00")
+            wf = scheduler.make_window_filter(ini, end)
+            f = list(filter(wf, data))
+            assert(len(f) == 1)
 
-      ini = scheduler.str_to_time("2020-11-10 09:00:00")
-      end = scheduler.str_to_time("2020-11-10 12:00:00")
-      wf = scheduler.make_window_filter(ini, end)
-      f = list(filter(wf, data))
-      assert(len(f) == 0)
+            ini = scheduler.str_to_time("2020-11-10 09:00:00")
+            end = scheduler.str_to_time("2020-11-10 12:00:00")
+            wf = scheduler.make_window_filter(ini, end)
+            f = list(filter(wf, data))
+            assert(len(f) == 0)
 
-  def test_sum_until_max(self):
-    with open('jobs.json') as json_file:
-      data = json.load(json_file)
+    def test_sum_until_max(self):
+        with open('jobs.json') as json_file:
+            data = json.load(json_file)
 
-      for i in data:
-        i['Tempo estimado'] = scheduler.str_natural_to_time(
-            i['Tempo estimado'])
+            for i in data:
+                i['Tempo estimado'] = scheduler.str_natural_to_time(
+                    i['Tempo estimado'])
 
-      max_time = scheduler.str_natural_to_time('8 horas')
-      l = scheduler.sum_until_max(data, max_time['datetime'])
-      assert(len(l) == 2)
+            max_time = scheduler.str_natural_to_time('8 horas')
+            l = scheduler.sum_until_max(data, max_time['datetime'])
+            assert(len(l) == 2)
 
-      max_time = scheduler.str_natural_to_time('1 horas')
-      l = scheduler.sum_until_max(data, max_time['datetime'])
-      assert(len(l) == 0)
+            max_time = scheduler.str_natural_to_time('1 horas')
+            l = scheduler.sum_until_max(data, max_time['datetime'])
+            assert(len(l) == 0)
 
-      max_time = scheduler.str_natural_to_time('23 horas e 59 minutos')
-      l = scheduler.sum_until_max(data, max_time['datetime'])
-      assert(len(l) == 3)
+            max_time = scheduler.str_natural_to_time('23 horas e 59 minutos')
+            l = scheduler.sum_until_max(data, max_time['datetime'])
+            assert(len(l) == 3)
 
 
 if __name__ == "__main__":
