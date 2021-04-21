aabd75ceab5766e568635627d5bb4da94dcd900f
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-04-20 21:46:37 -0300

    docs(python): create python docs

--- scheduler.py
+++ scheduler.py
@@ -7,11 +7,34 @@ pp = pprint.PrettyPrinter()
 
 
 def str_to_time(strtime):
+  """
+  Convert str('%Y-%m-%d %H:%M:%S') to datetime.
+
+  Parameters:
+  strtime (str): date and time
+
+  Returns:
+  datetime: Object datetime
+  """
   return datetime.strptime(strtime, '%Y-%m-%d %H:%M:%S')
 
 
-# 2 horas e 55 minutos
 def str_natural_to_time(str):
+  """
+  Convert natural brazilian time string to dict.
+
+  eg. (2 horas e 30 minutos, 1 hora, 8 horas,...)
+
+  Parameters:
+  str (str): Brazilian natural hours
+
+  Returns:
+  dict:
+    int: hours
+    int: minutes
+    int: seconds
+    datetime: Object datetime
+  """
   hours = 0
   minutes = 0
   seconds = 0
@@ -32,7 +55,6 @@ def str_natural_to_time(str):
     except:
       pass
 
-  # return datetime.strptime(f"{hours}:{minutes}:{seconds}", "%H:%M:%S")
   return {
       'hours': hours,
       'minutes': minutes,
@@ -46,6 +68,15 @@ end = str_to_time("2019-11-11 12:00:00")
 
 
 def scheduler_windows_filter(item):
+  """
+  Filter jobs between ini and end execution window.
+
+  Parameters:
+  item(dict): single job dict
+
+  Returns:
+  Boolean: True if the job is in window execution interval, otherwise false.
+  """
   if item['Data Máxima de conclusão'] >= ini and item['Data Máxima de conclusão'] <= end:
     return True
 
@@ -53,7 +84,16 @@ def scheduler_windows_filter(item):
 
 
 def sum_until_max(slice, max):
+  """
+  Sum slice of jobs and break if is lt max
 
+  Parameters:
+  slice (array): List of jobs
+  max (dict): Max windows execution
+
+  Returns:
+  array: Array with jobs ids (daily window)
+  """
   hours = 0
   minutes = 0
   seconds = 0
@@ -67,9 +107,10 @@ def sum_until_max(slice, max):
 
     t = datetime.strptime(f"{hours}:{minutes}:{seconds}", "%H:%M:%S")
 
-    result.append(item['ID'])
-    if t >= max:
-      return list(result)
+    if t <= max:
+      result.append(item['ID'])
+    else:
+      break
 
   return result
 
@@ -78,14 +119,18 @@ if __name__ == "__main__":
   with open('jobs.json') as json_file:
     data = json.load(json_file)
 
-  # fix data
+  # normalize data
   for i in data:
     i['Data Máxima de conclusão'] = str_to_time(i['Data Máxima de conclusão'])
     i['Tempo estimado'] = str_natural_to_time(i['Tempo estimado'])
 
+  # sort data by 'Data Máxima de conclusão'
   data = sorted(data, key=lambda k: k['Data Máxima de conclusão'])
 
+  # generate initial max_time like docs
   max_time = str_natural_to_time('8 horas')
+
+  # remove jobs outside execution window
   filtered = list(filter(scheduler_windows_filter, data))
 
   out = []
